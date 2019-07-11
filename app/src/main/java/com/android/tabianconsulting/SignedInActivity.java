100838b3c8514bab35d23047e717fef76d3251a9
segunfrancis <francis4dblues@gmail.com>
2019-07-11 06:20:29 +0100

    implementation: Email update

--- app/src/main/java/com/android/tabianconsulting/SignedInActivity.java
+++ app/src/main/java/com/android/tabianconsulting/SignedInActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignedInActivity extends AppCompatActivity {
 
@@ -27,6 +30,27 @@ public class SignedInActivity extends AppCompatActivity {
 
         setupFirebaseAuth();
         getUserDetails();
+
+        //setUserDetails();
+    }
+
+    private void setUserDetails() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName("Segun Francis")
+                    .setPhotoUri(Uri.parse("https://i.pinimg.com/originals/2d/d3/61/2dd361a57445e7141cd7475d4f5e463f.jpg"))
+                    .build();
+            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: User profile updated.");
+                        getUserDetails();
+                    }
+                }
+            });
+        }
     }
 
     private void getUserDetails() {
